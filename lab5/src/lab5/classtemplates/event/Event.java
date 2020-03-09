ee23e5a4c63e1781ff62cb316b8dde5ab66ffbb9
urbut1997 <patrik.grund97@gmail.com>
2020-03-09 13:29:32 +0100

    added some timers and kund to event and ankomstevent

--- lab5/src/lab5/classtemplates/event/Event.java
+++ lab5/src/lab5/classtemplates/event/Event.java
@@ -2,6 +2,8 @@ package lab5.classtemplates.event;
 
 import java.util.Observable;
 
+import lab5.Kund;
+
 /**
  * Represents different events in a concrete way
  * 
@@ -10,6 +12,8 @@ import java.util.Observable;
  */
 public abstract class Event extends Observable{
 	private int executionTime;
+	public int time;
+	public Kund kund;
 	public Event() {}
 	
 	public abstract void execute();
--- lab5/src/lab5/event/subevents/AnkomstEvent.java
+++ lab5/src/lab5/event/subevents/AnkomstEvent.java
@@ -1,13 +1,19 @@
 package lab5.event.subevents;
 
+import lab5.Kund;
 import lab5.classtemplates.event.Event;
 
 public class AnkomstEvent extends Event{
 
-	
+	public AnkomstEvent(Kund kund) {
+		// kanske ha med att kolla max antal kunder här
+		time = kund.ankomstTid;
+		super.kund=kund;
+	}
 
 	public void execute() {
 		// TODO Auto-generated method stub
+		kund.currentEvent = new PlockEvent(kund);
 		
 	}
 	
