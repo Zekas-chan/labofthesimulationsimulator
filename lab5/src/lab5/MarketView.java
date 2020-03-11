bec2a2a4621608f485f75779c87acd8afe612486
Zekas <Zekas@Providence>
2020-03-11 17:26:59 +0100

    Första utkastet för MarketView. Inget fungerar, än.

---
+++ lab5/src/lab5/MarketView.java
@@ -0,0 +1,68 @@
+package lab5;
+
+import java.awt.Event;
+import java.util.Observable;
+import java.util.Observer;
+
+import lab5.classtemplates.view.View;
+import lab5.state.MarketState;
+
+/**
+ * Concrete view of the program
+ * 
+ * @author 
+ *
+ */
+public class MarketView extends View implements Observer {
+	MarketState ms;
+	public MarketView(MarketState ms){
+		this.ms = ms;
+		ms.addObserver(this);
+		initiatePrinting(); //skriver ut raden som indikerar vad respektive informationssnutt är
+	}
+	
+	/*
+	 * Utskriftsmall:
+	 * Tid Händelse  Kund  Öppet/Stängt  led    ledT    I     $    :-(   köat    köT   köar  [Kassakö..]
+	 */
+	public void update(Observable o, Object arg) throws ClassCastException {
+		if(ms.isRunning()) {
+			eventDetails((Event)arg);
+		}else {
+			results();
+		}
+	}
+	
+	private void initiatePrinting() {
+		System.out.println("Tid		Händelse	Kund	Ö/S		led		ledT	I	$	:-(		köat	köT		köar	[Kassakö...]");
+	}
+	
+	private void eventDetails(Event a) {
+		System.out.print(ms.globalTime+"	"+a.toString()+"	"+a.Kund.getIndex()+"	"+isOpen()+"	"+ms.öppnaKassor()+"	"+ms.tidOverksamKassa+"	"+ms.getKundantal+"	"+ms.antalGenomfördaKöp+"	"+ms.antalMissadeKunder+"	"+ms.unikaKöandeKunder+"	"+ms.tidKassaKö+"	"+ms.KassaKö.length+"	"+köTillSträng());
+	}
+	
+	private String isOpen() {
+		if(ms.butikÖppen) {
+			return "Ö";
+		}else {
+			return "S";
+		}
+	}
+	
+	private String köTillSträng() {
+		String arraystr = "[";
+		for(int i = 0; i < ms.KassaKö.length; i++) {
+			arraystr += KassaKö[i].kund.getIndex()+", ";
+		}
+		arraystr += "]";
+		return arraystr;
+	}
+	
+	public void results() {
+		System.out.println("RESULTAT\n======\n \n");
+		System.out.println("1) Av "+ms.unikaKunder+" kunder handlade "+ms.antalGenomfördaKöp+" medan "+ms.antalMissadeKunder+" missades.\n");
+		System.out.println("Total tid "+ms.antalKassor+" varit lediga: "+ms.tidOverksamKassa+"\n");
+		System.out.println("3) Total tid "+ms.unikaKöandeKunder+" tvingats köa: "+ms.tidKassaKö+". \n Genomsnittlig kötid: "+(ms.tidKassaKö/ms.unikaKöandeKunder+"\n"));
+	}
+
+}
