97d97289fa853d7875b9692d0673cd337b3b2ad1
Zekas <Zekas@Providence>
2020-03-12 13:44:11 +0100

    Små uppdateringar till printouts samt kommentarer.
    La även till javadockommentarer för alla metoder, samt omformatterade
    textprintouts något.

--- lab5/src/lab5/MarketView.java
+++ lab5/src/lab5/MarketView.java
@@ -9,70 +9,91 @@ import lab5.event.MarketEvent;
 import lab5.state.MarketState;
 
 /**
- * Concrete view of the program
- * WORK IN PROGRESS
- * @author 
+ * Concrete view of the program.
+ * 
+ * @author
  *
  */
 public class MarketView extends View implements Observer {
 	MarketState ms;
 	boolean optimizationMode;
-	
-	public MarketView(MarketState ms, boolean mode){
+	/**
+	 * Konstruerar en MarketView som observerar en MarketState.
+	 * @param ms Referens till ett MarketState
+	 * @param mode Om satt till true skrivs bara resultatutskriften ut.
+	 */
+	public MarketView(MarketState ms, boolean mode) {
 		this.ms = ms;
 		ms.addObserver(this);
 		optimizationMode = mode;
-		if(!optimizationMode) {
-			initiatePrinting(); //skriver ut raden som indikerar vad respektive informationssnutt är
+		if (!optimizationMode) {
+			initiatePrinting(); // skriver ut raden som indikerar vad respektive informationssnutt är
 		}
-		
+
 	}
-	
-	/*
-	 * Utskriftsmall:
-	 * Tid Händelse  Kund  Öppet/Stängt  led    ledT    I     $    :-(   köat    köT   köar  [Kassakö..]
+
+	/**
+	 * 
 	 */
 	public void update(Observable o, Object arg) throws ClassCastException {
-		if(ms.isRunning() && !optimizationMode) {
-			eventDetails((MarketEvent)arg);
-		}else if (!ms.isRunning()) {
+		if (ms.isRunning() && !optimizationMode) {
+			eventDetails((MarketEvent) arg);
+		} else if (!ms.isRunning()) {
 			results();
 		}
 	}
-	
+	/**
+	 * Hjälpmetod för Update
+	 */
 	private void initiatePrinting() {
-		System.out.println("Tid		Händelse	Kund	Ö/S		led		ledT	I	$	:-(		köat	köT		köar	[Kassakö...]");
+		System.out.println("Tid\tHändelse\tKund\tÖ/S\tled\tledT\tI\t$\t:-(\tköat\tköT\tköar\t[Kassakö...]");
 	}
-	
+	/**
+	 * Hjälpmetod för update. Tar ett MarketEvent som argument för att kunna veta dess typ.
+	 * @param a Ett MarketEvent. 
+	 */
 	private void eventDetails(MarketEvent a) {
-		System.out.print(ms.globalTime+"	"+a.toString()+"	"+a.kund.id+"	"+isOpen()+"	"+ms.öppnaKassor()+"	"+ms.tidOverksamKassa+"	"+ms.kunderIButiken.size()+"	"+ms.antalGenomfördaKöp+"	"+ms.antalMissadeKunder+"	"+ms.unikaKöandeKunder+"	"+ms.tidKassaKö+"	"+ms.kassaKö.size()+"	"+köTillSträng());
+		System.out.print(ms.globalTime + "\t" + a.toString() + "\t" + a.kund.id + "\t" + isOpen() + "\t"
+				+ ms.öppnaKassor() + "\t" + ms.tidOverksamKassa + "\t" + ms.kunderIButiken.size() + "\t"
+				+ ms.antalGenomfördaKöp + "\t" + ms.antalMissadeKunder + "\t" + ms.unikaKöandeKunder + "\t"
+				+ ms.tidKassaKö + "\t" + ms.kassaKö.size() + "\t" + köTillSträng());
 	}
 	
+	/**
+	 * Hjälpmetod för Update.
+	 * @return Ö om det är öppet, annars S för stängt.
+	 */
 	private String isOpen() {
-		if(ms.öppet) {
+		if (ms.öppet) {
 			return "Ö";
-		}else {
+		} else {
 			return "S";
 		}
 	}
-	
+	/**
+	 * Hjälpmetod till update. Läser igenom kassakön och gör den till en array i en String.
+	 * @return En Sträng med innehållet utformat som en array.
+	 */
 	private String köTillSträng() {
 		String arraystr = "[";
-		for(int i = 0; i < ms.kassaKö.size(); i++) {
+		for (int i = 0; i < ms.kassaKö.size(); i++) {
 			arraystr += ms.kassaKö.get(i).id;
-			if(i < ms.kassaKö.size()-1) {
+			if (i < ms.kassaKö.size() - 1) {
 				arraystr += ", ";
 			}
 		}
 		arraystr += "]";
 		return arraystr;
 	}
-	
+	/**
+	 * Hjälpmetod för update. Om simuleringen kört klart skrivs simuleringens statistikvariabler ut.
+	 */
 	public void results() {
-		System.out.println("RESULTAT\n======\n \n");
-		System.out.println("1) Av "+ms.unikaKunder+" kunder handlade "+ms.antalGenomfördaKöp+" medan "+ms.antalMissadeKunder+" missades.\n");
-		System.out.println("Total tid "+ms.antalKassor+" varit lediga: "+ms.tidOverksamKassa+"\n");
-		System.out.println("3) Total tid "+ms.unikaKöandeKunder+" tvingats köa: "+ms.tidKassaKö+". \n Genomsnittlig kötid: "+(ms.tidKassaKö/ms.unikaKöandeKunder+"\n"));
+		System.out.println("RESULTAT\n========\n \n");
+		System.out.println("1) Av " + ms.unikaKunder + " kunder handlade " + ms.antalGenomfördaKöp + " medan "
+				+ ms.antalMissadeKunder + " missades.\n");
+		System.out.println("2) Total tid " + ms.antalKassor + " kassor varit lediga: " + ms.tidOverksamKassa + " minuter.\n");
+		System.out.println("3) Total tid " + ms.unikaKöandeKunder + " tvingats köa: " + ms.tidKassaKö +" minuter."
+				+ ". \n Genomsnittlig kötid: " + (ms.tidKassaKö / ms.unikaKöandeKunder + "\n"));
 	}
-
 }
