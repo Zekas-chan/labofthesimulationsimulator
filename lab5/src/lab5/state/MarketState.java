d98f1b72a8a1c8af0c589501ea91e018e0e75046
urbut1997 <patrik.grund97@gmail.com>
2020-06-01 16:45:07 +0200

    Added javadocs to getters and setters

--- lab5/src/lab5/state/MarketState.java
+++ lab5/src/lab5/state/MarketState.java
@@ -47,22 +47,24 @@ public class MarketState extends State {
 	private int ledigaKassor;
 	private boolean öppnaKassor;
 	private boolean öppet;
-	private UniformRandomStream rM; //betalningar
-	private UniformRandomStream rMB; //plock
-	private ExponentialRandomStream rE; //ankomster
-	
+	private UniformRandomStream rM; // betalningar
+	private UniformRandomStream rMB; // plock
+	private ExponentialRandomStream rE; // ankomster
+
 	/**
 	 * Konstruerar ett nytt MarketState.
 	 * 
-	 * @param öppetTider	Anger hur länge butiken är öppen.
-	 * @param kassor		Anger hur många kassor som är bemannade.
-	 * @param ankomstLambda	Anger i hur snabb takt kunder anländer.
-	 * @param frö			Fröet för slumpningstider som används.
-	 * @param maxKunder		Maximalt antal kunder som kan vistas i butiken.
-	 * @param plocktid		Anger ett intervall för hur länge det tar att plocka varor.
-	 * @param betaltid		Anger ett intervall för hur länge det tar att betala.
+	 * @param öppetTider    Anger hur länge butiken är öppen.
+	 * @param kassor        Anger hur många kassor som är bemannade.
+	 * @param ankomstLambda Anger i hur snabb takt kunder anländer.
+	 * @param frö           Fröet för slumpningstider som används.
+	 * @param maxKunder     Maximalt antal kunder som kan vistas i butiken.
+	 * @param plocktid      Anger ett intervall för hur länge det tar att plocka
+	 *                      varor.
+	 * @param betaltid      Anger ett intervall för hur länge det tar att betala.
 	 */
-	public MarketState(double öppetTider, int kassor, double ankomstLambda, int frö, int maxKunder, double[] plocktid, double[] betaltid) {
+	public MarketState(double öppetTider, int kassor, double ankomstLambda, int frö, int maxKunder, double[] plocktid,
+			double[] betaltid) {
 		/*
 		 * Parameterblock
 		 */
@@ -92,14 +94,14 @@ public class MarketState extends State {
 		ledigaKassor = antalKassor;
 		öppnaKassor = true;
 		setÖppet(true);
-		
+
 		/*
 		 * Initiering av slumpmotorer.
 		 */
 		rM = new UniformRandomStream(plockTid[0], plockTid[1], frö);
 		rMB = new UniformRandomStream(betalTid[0], betalTid[1], frö);
 		rE = new ExponentialRandomStream(ankomstLambda, frö);
-		
+
 		/*
 		 * Skapar händelsekön och lägger till ett StartEvent.
 		 */
@@ -107,7 +109,7 @@ public class MarketState extends State {
 		CustFac = new CustomerFactory();
 		new StartEvent(this, eq, CustFac);
 	}
-	
+
 	/**
 	 * Startar simuleringen genom att köra första eventet i kön (StartEvent).
 	 */
@@ -139,114 +141,211 @@ public class MarketState extends State {
 		setChanged();
 		notifyObservers(e);
 	}
-	
-	//Setters
+
+	// Setters
+	/**
+	 * 
+	 * @param kunderIButiken
+	 */
 	public void setKunderIButiken(ArrayList<Kund> kunderIButiken) {
 		this.kunderIButiken = kunderIButiken;
 	}
-	
+
+	/**
+	 * styr ifall butik är öppen.
+	 * 
+	 * @param öppet
+	 */
 	public void setÖppet(boolean öppet) {
 		this.öppet = öppet;
 	}
-	
+
+	/**
+	 * ökar antalet kunder som har varit i butiken
+	 */
 	public void incUnikaKunder() {
 		unikaKunder++;
 	}
-	
+
+	/**
+	 * ökar antalet kunder som stått kö
+	 */
 	public void incUnikaKöandeKunder() {
 		unikaKöandeKunder++;
 	}
-	
+
+	/**
+	 * ökar antalet lediga kassor
+	 * 
+	 * @param antal
+	 */
 	public void incLedigaKassor(int antal) {
 		ledigaKassor += antal;
 	}
-	
+
+	/**
+	 * minskar antalet lediga kassor
+	 */
 	public void decLedigaKassor() {
 		ledigaKassor--;
 	}
-	
+
+	/**
+	 * ökar antalet missade kunder
+	 */
 	public void incMissadeKunder() {
 		antalMissadeKunder++;
 	}
-	
+
+	/**
+	 * ökar antalet genomförda köp
+	 */
 	public void incAntalGenomfördaKöp() {
 		antalGenomfördaKöp++;
 	}
-	
+
+	/**
+	 * ökar antalet kötid
+	 * 
+	 * @param time
+	 */
 	public void addTidKassaKö(double time) {
 		tidKassaKö += time;
 	}
-	
+
+	/**
+	 * ökar tid som kassor stått tomma
+	 * 
+	 * @param time
+	 */
 	public void addTidOverksamKassa(double time) {
 		tidOverksamKassa += time;
 	}
-	
+
+	/**
+	 * sätt tid som senaste betalninghändelse var
+	 * 
+	 * @param time
+	 */
 	public void setFinalPaymentEvent(double time) {
 		finalPaymentEvent = time;
 	}
-	
-	//======================================================
-	//Getters
+
+	// ======================================================
+	// Getters
+	/**
+	 * hämtar antalet genomförda köp
+	 * 
+	 * @return
+	 */
 	public int getAntalGenomfördaKöp() {
 		return antalGenomfördaKöp;
 	}
-	
+
+	/**
+	 * hämtar tid kassor stått tomma
+	 * 
+	 * @return
+	 */
 	public double getTidOverksamKassa() {
 		return tidOverksamKassa;
 	}
-	
+
+	/**
+	 * hämtar antalet lediga kassor
+	 * 
+	 * @return
+	 */
 	public int getledigaKassor() {
 		return ledigaKassor;
 	}
-	
+
+	/**
+	 * hämtar snabbköptets öppettider
+	 * 
+	 * @return
+	 */
 	public double getSnabbKöpsÖppettider() {
 		return snabbKöpsÖppettider;
 	}
 
+	/**
+	 * hämtar listan med kunder i butiken
+	 * 
+	 * @return
+	 */
 	public ArrayList<Kund> getKunderIButiken() {
 		return kunderIButiken;
 	}
-	
+
+	/**
+	 * hämmtar om affären är öpen
+	 * 
+	 * @return
+	 */
 	public boolean isÖppet() {
 		return öppet;
 	}
-	
+
+	/**
+	 * hämtar antalet missade kunder
+	 * 
+	 * @return
+	 */
 	public int getAntalMissadeKunder() {
 		return antalMissadeKunder;
 	}
-	
+
+	/**
+	 * hämtar antalet köande kunder
+	 * 
+	 * @return
+	 */
 	public int getUnikaKöandeKunder() {
 		return unikaKöandeKunder;
 	}
-	
+
+	/**
+	 * hämtar antalet kunder
+	 * 
+	 * @return
+	 */
 	public int getUnikaKunder() {
 		return unikaKunder;
 	}
-	
+
+	/**
+	 * hämtar tid som köats
+	 * 
+	 * @return
+	 */
 	public double getTidKassaKö() {
 		return tidKassaKö;
 	}
-	
+
 	/**
 	 * Genererar en betaltid.
+	 * 
 	 * @return
 	 */
 	public double getBetalTid() {
 		double rmB = rMB.nextDouble();
 		return rmB;
 	}
-	
+
 	/**
 	 * Genererar en plocktid.
+	 * 
 	 * @return
 	 */
 	public double getPlockTid() {
 		double rm = rM.nextDouble();
 		return rm;
 	}
-	
+
 	/**
 	 * Genererar en ankomsttid.
+	 * 
 	 * @return
 	 */
 	public double getAnkomst() {
@@ -254,10 +353,20 @@ public class MarketState extends State {
 		return re;
 	}
 
+	/**
+	 * hämtar en lista med kunder i kassakön
+	 * 
+	 * @return
+	 */
 	public ArrayList<Kund> getKassaKö() {
 		return kassaKö;
 	}
 
+	/**
+	 * hämtar senaste betala eventets tid
+	 * 
+	 * @return
+	 */
 	public double getFinalPaymentEvent() {
 		return finalPaymentEvent;
 	}
